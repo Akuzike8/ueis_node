7f11e726b1137dcb19c6c2dd54f645afbcfe207b
Akuzike8 <akuzikenchembe8@gmail.com>
2023-09-23 12:41:54 +0200

    ueis_serialport_working

--- static/javascript/serialport.js
+++ static/javascript/serialport.js
@@ -78,27 +78,5 @@ class SerialPortHandler {
         navigator.serial.addEventListener('disconnect', this.onDisconnect);
     }
 }
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-document.getElementById('scan-btn').addEventListener("click", async () => {
-    const port = new SerialPortHandler(
-        { baudRate: 9600},
-        () => console.log("Device connected"),
-        () => console.log("Device disconnected."),
 
-    )
-
-    const info = await port.open()
-   // var message = await port.read()
-   // message = message.replace(/\s/g, "")
-
-   // avoids the writing before the arduino is up and running
-   sleep(2000).then(async () => {
-        await port.write("1");
-        const message = await port.read();
-        console.log(message);
-    });
-
-})
